11f5dd8f4349c775d4f766760012ea8bf0360b5b
jossnock <19121867@hlnsc.ac.uk>
2024-02-02 15:27:03 +0000

    upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function loaded() {
     console.log("The page has now loaded");
 }
+
+function showCorrect() {
+    document.getElementById("feedback-area").innerHTML = "Correct"
+}
+
+function showIncorrect() {
+document.getElementById("feedback-area").innerHTML = "Incorrect"
+}
