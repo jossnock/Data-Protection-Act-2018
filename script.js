23d5e5b6627ede70f07d2e8fd62071d200afd904
jossnock <19121867@hlnsc.ac.uk>
2024-02-02 15:39:16 +0000

    added waiting

--- script.js
+++ script.js
@@ -2,10 +2,16 @@ function loaded() {
     console.log("The page has now loaded");
 }
 
+function showNothing() {
+    document.getElementById("feedback-area").innerHTML = "";
+}
+
 function showCorrect() {
-    document.getElementById("feedback-area").innerHTML = "Correct"
+    document.getElementById("feedback-area").innerHTML = "Correct";
+    const myTimeout = setTimeout(showNothing, 5000);
 }
 
 function showIncorrect() {
 document.getElementById("feedback-area").innerHTML = "Incorrect"
+const myTimeout = setTimeout(showNothing, 5000);
 }
