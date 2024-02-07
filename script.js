23f41a74a2b5718bdeff0cc2dc578e292c6cda27
jossnock <144459459+jossnock@users.noreply.github.com>
2024-02-07 17:46:51 +0000

    todo: finish quiz

--- script.js
+++ script.js
@@ -18,29 +18,36 @@ const myTimeout = setTimeout(showNothing, 5000);
 
 
 //experimenting:
+
+const questions = {
+    "question1": {
+        "question": "Which of the following is not covered by the DPA (2018):",
+        "answers": {
+            "a": "Biometrics",
+            "b": "Political Opinions",
+            "c": "Occupation"
+        },
+        "correctAnswer": "c"
+    },
+    "question2": {
+        "question": "For ordinary circumstances, how long should it take for an organisation to provide requested information",
+        "answers": {
+            "a": "1 week",
+            "b": "2 months",
+            "c": "6 months"
+        },
+        "correctAnswer": "b"
+    }
+};
+
 function showQuestions(){
-    var questions = [
-        {
-            question: "Which of the following is not covered by the DPA (2018):",
-            answers: {
-                a: "Biometrics",
-                b: "Political Opinions",
-                c: "Occupation"
-            },
-            correctAnswer: "c"
-        },
-        {
-            question: "For ordinary circumstances, how long should it take for an organisation to provide requested information",
-            answers: {
-                a: "1 week",
-                b: "2 months",
-                c: "6 months"
-            },
-            correctAnswer: "b"
-        },
-    ];
+    document.getElementById("quiz-area").innerHTML = questions["question1"]["question"]
+    document.getElementById("quiz-area").innerHTML +=  "<br>a: " + questions["question1"]["answers"]["a"]
+    document.getElementById("quiz-area").innerHTML +=  "<br>b: " + questions["question1"]["answers"]["b"]
+    document.getElementById("quiz-area").innerHTML +=  "<br>c: " + questions["question1"]["answers"]["c"]
 
 }
+
 function showResults(){
     
 }
