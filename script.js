5f3ce5453592b40bef9f9894c6d7b6ece79eea93
jossnock <144459459+jossnock@users.noreply.github.com>
2024-02-07 15:58:13 +0000

    wefwef

--- script.js
+++ script.js
@@ -15,3 +15,34 @@ function showIncorrect() {
 document.getElementById("feedback-area").innerHTML = "Incorrect"
 const myTimeout = setTimeout(showNothing, 5000);
 }
+
+
+//experimenting:
+function showQuestions(){
+    var questions = [
+        {
+            question: "Which of the following is not covered by the DPA (2018):",
+            answers: {
+                a: "Biometrics",
+                b: "Political Opinions",
+                c: "Occupation"
+            },
+            correctAnswer: "c"
+        },
+        {
+            question: "For ordinary circumstances, how long should it take for an organisation to provide requested information",
+            answers: {
+                a: "1 week",
+                b: "2 months",
+                c: "6 months"
+            },
+            correctAnswer: "b"
+        },
+    ];
+
+}
+function showResults(){
+    
+}
+
+showQuestions();
